6b2883adf963b416f37af0e5afd60180b0509990
Mehdi Kord <mehdi.kord171@gmail.com>
2025-03-12 03:13:10 +0330

    upgrade

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -4,7 +4,7 @@
 import axios from "axios";
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.baseURL="http://localhost:8000";
+window.axios.defaults.baseURL="https://core.pergola.ir";
 window.axios.interceptors.response.use(
     response => {
 
