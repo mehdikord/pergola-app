7e2a7207aee858ec4b6295818fb4b49899310349
Mehdi Kord <mehdi.kord171@gmail.com>
2025-03-12 04:03:54 +0330

    upgrade

--- vite.config.js
+++ vite.config.js
@@ -23,12 +23,12 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/icons/icon-192x192.png',
+            src: '/icons/icon.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/icons/icon-512x512.png',
+            src: '/icons/icon.png',
             sizes: '512x512',
             type: 'image/png',
           },
