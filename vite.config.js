141b72f403decd736ee6d16d98f7e0ddebff63fa
Mehdi Kord <mehdi.kord171@gmail.com>
2025-03-12 13:47:13 +0330

    upgrade

--- vite.config.js
+++ vite.config.js
@@ -1,59 +1,66 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
-import { VitePWA } from 'vite-plugin-pwa';
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: { transformAssetUrls }
+    }),
     quasar({
       sassVariables: 'src/quasar-variables.sass'
     }),
     VitePWA({
-      registerType: 'autoUpdate', // سرویس‌ورکر به‌صورت خودکار آپدیت می‌شه
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      strategies: 'generateSW',
       manifest: {
         name: 'Pergola',
         short_name: 'Pergola',
-        description: '',
+        description: '...',
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
         start_url: '/',
+        orientation: 'portrait',
+        scope: '/',
         icons: [
           {
-            src: '/icon-192x192.png',
+            src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
+            purpose: 'any maskable'
           },
           {
-            src: '/icon-512x512.png',
+            src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-          },
-        ],
+            purpose: 'any maskable'
+          }
+        ]
       },
       workbox: {
-        // فایل‌هایی که می‌خوای آفلاین در دسترس باشن
-
+        globPatterns: ['**/*.{js,css,html,png,svg,ico,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: ({request}) => request.destination === 'document',
+            handler: 'NetworkFirst',
+          },
+          {
+            urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp)$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'images-cache',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+              },
+            },
+          }
+        ]
       },
       devOptions: {
-        enabled: true, // فعال کردن PWA توی حالت توسعه
-      },
-    }),
+        enabled: true,
+        type: 'module',
+        navigateFallback: 'index.html'
+      }
+    })
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
-    },
-    mainFields: [
-      'browser',
-      'module',
-      'main',
-      'jsnext:main',
-      'jsnext'
-    ]
-  }
+  // ...بقیه تنظیمات
 })
