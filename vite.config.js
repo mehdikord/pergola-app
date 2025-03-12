79f56c2480388427eaae3be8ffde47bc9f2c80ec
Mehdi Kord <mehdi.kord171@gmail.com>
2025-03-12 13:48:57 +0330

    upgrade

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import { VitePWA } from 'vite-plugin-pwa';
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({
-      template: { transformAssetUrls }
-    }),
+    vue(),
     quasar({
       sassVariables: 'src/quasar-variables.sass'
     }),
@@ -62,5 +67,17 @@ export default defineConfig({
       }
     })
   ],
-  // ...بقیه تنظیمات
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+    },
+    mainFields: [
+      'browser',
+      'module',
+      'main',
+      'jsnext:main',
+      'jsnext'
+    ]
+  }
 })
